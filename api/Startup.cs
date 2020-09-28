f23930cb8d166bbcf2646af5db5448b630323206
Романов Роман <romanovmailbox@mail.ru>
2020-09-28 20:07:04 +0300

    remove unused imports

--- api/Startup.cs
+++ api/Startup.cs
@@ -2,7 +2,6 @@ using BLL;
 using DAL;
 using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
-using Microsoft.AspNetCore.Http;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
