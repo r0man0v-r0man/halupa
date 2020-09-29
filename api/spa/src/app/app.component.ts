9765453a2c69fd1f2bb826c01fec0d5bc0dfe690
Романов Роман <romanovmailbox@mail.ru>
2020-09-29 09:46:17 +0300

    update app component

--- api/spa/src/app/app.component.ts
+++ api/spa/src/app/app.component.ts
@@ -2,7 +2,7 @@ import { Component } from '@angular/core';
 
 @Component({
   selector: 'app-root',
-  template: `<mat-slider min="1" max="100" step="1" value="1"></mat-slider>`,
+  template: `<h1>{{title}}</h1>`,
   styles: []
 })
 export class AppComponent {
