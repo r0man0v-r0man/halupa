6d25ff80686de7a230913e732d5dfdb67fb23eb5
Романов Роман <romanovmailbox@mail.ru>
2020-12-07 11:42:38 +0300

    update styles advert page

--- spa/web-spa/src/app/pages/advert/advert.module.ts
+++ spa/web-spa/src/app/pages/advert/advert.module.ts
@@ -1,16 +1,12 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
-import { NzPageHeaderModule } from 'ng-zorro-antd/page-header';
 import { AdvertRoutingModule } from './advert-routing.module';
 import { AdvertComponent } from './advert.component';
-import { NzTagModule } from 'ng-zorro-antd/tag';
 import { NzButtonModule } from 'ng-zorro-antd/button';
 import { NzIconModule } from 'ng-zorro-antd/icon';
 import { IconDefinition } from '@ant-design/icons-angular';
 import { NzTypographyModule } from 'ng-zorro-antd/typography';
 import { ImageSliderModule } from 'src/app/modules/image-slider/image-slider.module';
-import { NzDescriptionsModule } from 'ng-zorro-antd/descriptions';
-import { NzStatisticModule } from 'ng-zorro-antd/statistic';
 import { NzModalModule } from 'ng-zorro-antd/modal';
 import { 
   PhoneOutline, 
@@ -26,7 +22,7 @@ const icons: IconDefinition[] = [
   CopyOutline, 
   EnvironmentOutline,
   ArrowLeftOutline,
-    SwapOutline
+  SwapOutline
 ];
 
 
@@ -37,17 +33,13 @@ const icons: IconDefinition[] = [
   imports: [
     CommonModule,
     AdvertRoutingModule,
-    NzPageHeaderModule,
-    NzTagModule,
     NzButtonModule,
     NzIconModule.forChild(icons),
     NzTypographyModule,
     ImageSliderModule,
-    NzDescriptionsModule,
-    NzStatisticModule,
     NzModalModule,
     YandexMapModule,
-      PriceModule
+    PriceModule
   ],
   exports: [
     AdvertComponent
