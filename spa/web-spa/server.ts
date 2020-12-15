9eab101d498531a0901908785fc1cbba0aca0044
Романов Роман <romanovmailbox@mail.ru>
2020-12-15 10:37:17 +0300

    remove unused axios config

--- spa/web-spa/server.ts
+++ spa/web-spa/server.ts
@@ -24,10 +24,7 @@ export function app(): express.Express {
   
   // Example Express Rest API endpoints
   server.get('*/sitemap**', (req, res) => {
-    const config = {
-      headers: {'Content-Type': 'text/xml'}
-    };
-    Axios.get('http://localhost:5000/sitemap.xml', config).then(sitemap => {
+    Axios.get('http://localhost:5000/sitemap.xml').then(sitemap => {
       res.setHeader('Content-Type','text/xml')
       res.send(sitemap.data)  
     })
