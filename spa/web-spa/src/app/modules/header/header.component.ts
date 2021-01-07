d8373e31eae850142ef01eeeba080a3a22fedb75
Романов Роман <romanovmailbox@mail.ru>
2021-01-07 23:33:46 +0300

    update header component

--- spa/web-spa/src/app/modules/header/header.component.ts
+++ spa/web-spa/src/app/modules/header/header.component.ts
@@ -1,4 +1,5 @@
-import { Component, OnInit } from '@angular/core';
+import { isPlatformBrowser } from '@angular/common';
+import { Component, Inject, OnInit, PLATFORM_ID } from '@angular/core';
 import { AuthService } from 'src/app/services/auth.service';
 
 @Component({
@@ -8,11 +9,24 @@ import { AuthService } from 'src/app/services/auth.service';
 })
 export class HeaderComponent implements OnInit {
   toggleMenu = false;
+  hideButtonsLabel:boolean;
   constructor(
-    public _authService: AuthService
+    public _authService: AuthService,
+    @Inject(PLATFORM_ID) private platformId: any
   ) { }
 
   ngOnInit(): void {
+    if(isPlatformBrowser(this.platformId)){
+      this.getHideButtons(window.innerWidth);
+      window.addEventListener('resize', () => {
+        this.getHideButtons(window.innerWidth);
+      })
+    }
+    
+  }
+
+  private getHideButtons(width: number) {
+    this.hideButtonsLabel = width < 800;
   }
 
   onToggle() {
