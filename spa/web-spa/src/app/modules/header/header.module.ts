834acf282a69c211a4bd57ae4f28cef7ade58386
Романов Роман <romanovmailbox@mail.ru>
2021-01-17 23:15:22 +0300

    init update dark styles

--- spa/web-spa/src/app/modules/header/header.module.ts
+++ spa/web-spa/src/app/modules/header/header.module.ts
@@ -14,6 +14,7 @@ import {
 } from '@ant-design/icons-angular/icons';
 import {SearchBoxModule} from "../search-box/search-box.module";
 import {LogoModule} from "../logo/logo.module";
+import { InlineSVGModule } from 'ng-inline-svg';
 const icons = [
   SearchOutline,
   PlusOutline,
@@ -30,6 +31,7 @@ const icons = [
     CommonModule,
     AppRoutingModule,
     NzIconModule.forChild(icons),
+    InlineSVGModule,
     NzButtonModule,
       SearchBoxModule,
       LogoModule
