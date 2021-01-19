07d27919988612e0632a57f64e4fa246a3d518bd
Романов Роман <romanovmailbox@mail.ru>
2021-01-19 23:09:59 +0300

    update home

--- spa/web-spa/src/app/modules/header/header.module.ts
+++ spa/web-spa/src/app/modules/header/header.module.ts
@@ -2,27 +2,9 @@ import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { HeaderComponent } from './header.component';
 import { AppRoutingModule } from 'src/app/app-routing.module';
-import { NzButtonModule } from 'ng-zorro-antd/button';
-import { NzIconModule } from 'ng-zorro-antd/icon';
-import {
-  PlusOutline,
-  LoginOutline,
-  UserOutline,
-    MenuOutline,
-    SearchOutline,
-    CloseOutline
-} from '@ant-design/icons-angular/icons';
 import {SearchBoxModule} from "../search-box/search-box.module";
-import {LogoModule} from "../logo/logo.module";
 import { InlineSVGModule } from 'ng-inline-svg';
-const icons = [
-  SearchOutline,
-  PlusOutline,
-  LoginOutline,
-  UserOutline,
-  MenuOutline,
-  CloseOutline
-]
+
 @NgModule({
   declarations: [
     HeaderComponent
@@ -30,11 +12,8 @@ const icons = [
   imports: [
     CommonModule,
     AppRoutingModule,
-    NzIconModule.forChild(icons),
     InlineSVGModule,
-    NzButtonModule,
-      SearchBoxModule,
-      LogoModule
+      SearchBoxModule
   ],
   exports:[
     HeaderComponent
