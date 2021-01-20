138e82c613265a5b6f9036bebaedd50f11679907
Романов Роман <romanovmailbox@mail.ru>
2021-01-20 22:00:27 +0300

    update footer

--- spa/web-spa/src/app/modules/footer/footer.module.ts
+++ spa/web-spa/src/app/modules/footer/footer.module.ts
@@ -1,13 +1,15 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { FooterComponent } from './footer.component';
+import { InlineSVGModule } from 'ng-inline-svg';
 
 @NgModule({
   declarations: [
     FooterComponent
   ],
   imports: [
-    CommonModule
+    CommonModule,
+    InlineSVGModule
   ],
   exports: [
     FooterComponent
