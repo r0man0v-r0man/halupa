44c4d1641ce7b560f592a75262f0e05923ef3283
Романов Роман <romanovmailbox@mail.ru>
2021-01-27 12:52:02 +0300

    add gsap animation

--- spa/web-spa/src/app/pages/home/home.component.ts
+++ spa/web-spa/src/app/pages/home/home.component.ts
@@ -1,15 +1,84 @@
-import { Component, OnInit } from '@angular/core';
+import { AfterViewInit, Component, ElementRef, OnInit, ViewChild } from '@angular/core';
+import { gsap } from 'gsap';
 
 @Component({
   selector: 'app-home',
   templateUrl: './home.component.html',
   styleUrls: ['./home.component.less']
 })
-export class HomeComponent implements OnInit {
-
-  constructor() { }
+export class HomeComponent implements OnInit, AfterViewInit {
+  @ViewChild('card') card: ElementRef;
+  @ViewChild('card_top') cardTop: ElementRef;
+  @ViewChild('card_icon') cardIcon: ElementRef;
+  @ViewChild('card_blip1') card_blip1: ElementRef;
+  @ViewChild('card_blip2') card_blip2: ElementRef;
+  @ViewChild('card_blip3') card_blip3: ElementRef;
+  @ViewChild('card_blip4') card_blip4: ElementRef;
+  @ViewChild('card_blip5') card_blip5: ElementRef;
+  constructor() { 
+  }
+  ngAfterViewInit(): void {
+    const card: Element = this.card.nativeElement;
+    const cardTop: Element = this.cardTop.nativeElement;
+    const cardIcon: Element = this.cardIcon.nativeElement;
+    const card_blip1: Element = this.card_blip1.nativeElement;
+    const card_blip2: Element = this.card_blip2.nativeElement;
+    const card_blip3: Element = this.card_blip3.nativeElement;
+    const card_blip4: Element = this.card_blip4.nativeElement;
+    const card_blip5: Element = this.card_blip5.nativeElement;
+    const timeline = gsap.timeline();
+    timeline
+    .from(card, {
+      duration: 1,
+      ease: "back.inOut(3)",
+      delay: 1,
+      css: {
+        opacity: 0,
+        scale: .2,
+        transformOrigin: 'center',
+      }
+    },"=.2")
+    .from(cardTop, {
+      css:{
+        scaleY: 0,
+        transformOrigin: 'top'
+      }
+    })
+    .from(cardIcon, {
+      css:{
+        scale: 0,
+        transformOrigin: 'center'
+      }
+    }, "-=.7")
+    .from(card_blip1,{
+      css:{
+        scaleX: 0
+      }
+    })
+    .from(card_blip2,{
+      css:{
+        scaleX: 0
+      }
+    }, "-=.2")
+    .from(card_blip3,{
+      css:{
+        scaleX: 0
+      }
+    }, "-=.3")
+    .from(card_blip4,{
+      css:{
+        scaleX: 0
+      }
+    }, "-=.6")
+    .from(card_blip5,{
+      css:{
+        scaleX: 0
+      }
+    }, "-=.7")
+  }
 
   ngOnInit(): void {
+    
   }
 
 }
