b09d154e239da020f9e2f1b7a94014f5ba4d7c3d
Романов Роман <romanovmailbox@mail.ru>
2021-01-28 20:29:19 +0300

    add animation to menu btn

--- spa/web-spa/src/app/modules/header/header.component.ts
+++ spa/web-spa/src/app/modules/header/header.component.ts
@@ -1,5 +1,4 @@
-import { isPlatformBrowser } from '@angular/common';
-import { AfterViewInit, Component, ElementRef, Inject, OnInit, PLATFORM_ID, ViewChild } from '@angular/core';
+import { Component, ElementRef, OnInit, ViewChild } from '@angular/core';
 import { AuthService } from 'src/app/services/auth.service';
 import { GsapService } from 'src/app/services/gsap.service';
 
@@ -11,22 +10,15 @@ import { GsapService } from 'src/app/services/gsap.service';
     GsapService
   ]
 })
-export class HeaderComponent implements OnInit, AfterViewInit {
+export class HeaderComponent implements OnInit {
   @ViewChild('logo') logo: ElementRef;
   constructor(
     public _authService: AuthService,
-    @Inject(PLATFORM_ID) private platformId: any,
     private _gsapService: GsapService
   ) { }
   onLogoClick(){
     this._gsapService.applyLogoAnimation(this.logo.nativeElement)
   }
-  ngAfterViewInit(): void {
-    // if(isPlatformBrowser(this.platformId)){
-    //   this._gsapService.applyLogoAnimation(this.logo.nativeElement)
-    // }
-  }
-
   ngOnInit(): void {
   }
 }
