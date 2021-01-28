96c1de357dc0d6344540d80edb47abb5b34b41bd
Романов Роман <romanovmailbox@mail.ru>
2021-01-28 21:36:33 +0300

    add navigation base functionalities

--- spa/web-spa/src/app/modules/header/header.component.ts
+++ spa/web-spa/src/app/modules/header/header.component.ts
@@ -1,4 +1,4 @@
-import { Component, ElementRef, OnInit, ViewChild } from '@angular/core';
+import { Component, ElementRef, OnInit, Renderer2, ViewChild } from '@angular/core';
 import { AuthService } from 'src/app/services/auth.service';
 import { GsapService } from 'src/app/services/gsap.service';
 
@@ -12,13 +12,30 @@ import { GsapService } from 'src/app/services/gsap.service';
 })
 export class HeaderComponent implements OnInit {
   @ViewChild('logo') logo: ElementRef;
+  @ViewChild('nav_links') nav_links: ElementRef;
+  @ViewChild('app_menu') app_menu: ElementRef;
+  isToggleMenu: boolean;
+
   constructor(
     public _authService: AuthService,
-    private _gsapService: GsapService
+    private _gsapService: GsapService,
+    private _renderer: Renderer2
   ) { }
   onLogoClick(){
     this._gsapService.applyLogoAnimation(this.logo.nativeElement)
+    this.onToggleMenu(true);
   }
   ngOnInit(): void {
+  }
+  onToggleMenu(isLogoClick?: boolean){
+    if(isLogoClick){
+      this.isToggleMenu = false;
+      this._renderer.setStyle(this.nav_links.nativeElement, 'display', 'none');
+      this._renderer.setStyle(this.app_menu.nativeElement, 'transform','unset')
+    } else {
+      this.isToggleMenu = !this.isToggleMenu;
+      this._renderer.setStyle(this.nav_links.nativeElement, 'display', this.isToggleMenu?'block':'none');
+      this._renderer.setStyle(this.app_menu.nativeElement, 'transform', this.isToggleMenu?'rotate(90deg)':'unset')
+    }
   }
 }
