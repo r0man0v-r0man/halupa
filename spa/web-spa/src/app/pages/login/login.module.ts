0c0b8fc4a2bf1ef726b3a4dee2791cc0a49fd739
Романов Роман <romanovmailbox@mail.ru>
2021-02-07 14:03:01 +0300

    login form style

--- spa/web-spa/src/app/pages/login/login.module.ts
+++ spa/web-spa/src/app/pages/login/login.module.ts
@@ -4,23 +4,6 @@ import { CommonModule } from '@angular/common';
 import { LoginRoutingModule } from './login-routing.module';
 import { LoginComponent } from './login.component';
 import { FormsModule, ReactiveFormsModule } from '@angular/forms';
-import { NzFormModule } from 'ng-zorro-antd/form';
-import { NzSpinModule } from 'ng-zorro-antd/spin';
-import { NzDividerModule } from 'ng-zorro-antd/divider';
-import { NzButtonModule } from 'ng-zorro-antd/button';
-import { NzInputModule } from 'ng-zorro-antd/input';
-import { NzTypographyModule } from 'ng-zorro-antd/typography';
-import { NzIconModule } from 'ng-zorro-antd/icon';
-import {
-  UserOutline,
-  LockOutline,
-  LoginOutline
-} from '@ant-design/icons-angular/icons';
-const icons = [
-  UserOutline,
-  LockOutline,
-  LoginOutline
-]
 
 @NgModule({
   declarations: [
@@ -30,14 +13,7 @@ const icons = [
     CommonModule,
     LoginRoutingModule,
     FormsModule,
-    ReactiveFormsModule,
-    NzFormModule,
-    NzSpinModule,
-    NzDividerModule,
-    NzButtonModule,
-    NzInputModule,
-    NzTypographyModule,
-    NzIconModule.forChild(icons)
+    ReactiveFormsModule
   ]
 })
 export class LoginModule { }
