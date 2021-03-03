9cdea5917eee44a1e18e07264f5b8551ded21ad2
Романов Роман <romanovmailbox@mail.ru>
2021-03-03 10:14:35 +0300

    update filelogger

--- api/Logging/FileLogger.cs
+++ api/Logging/FileLogger.cs
@@ -12,15 +12,9 @@ namespace api.Logging
             path = Path.Combine(folderPath, filePath.Replace("{date}", DateTime.Now.ToString("yyyyMMdd")));
         }
 
-        public IDisposable BeginScope<TState>(TState state)
-        {
-            return null;
-        }
+        public IDisposable BeginScope<TState>(TState state) => null;
 
-        public bool IsEnabled(LogLevel logLevel)
-        {
-            return logLevel != LogLevel.None;
-        }
+        public bool IsEnabled(LogLevel logLevel) => logLevel != LogLevel.None;
 
         public async void Log<TState>(LogLevel logLevel, EventId eventId, TState state, Exception exception, Func<TState, Exception, string> formatter)
         {
@@ -28,11 +22,10 @@ namespace api.Logging
             {
                 return;
             }
-            var logRecord = string.Format("{0} {1} {2}",
-                "[" + DateTime.Now + "]",
+            var logRecord = string.Format("{0} {1} {2} {3}",
+                "[" + DateTime.Now + "]", "[" + logLevel.ToString() + "]",
                 exception, formatter(state, exception));
-            await using var streamWriter = new StreamWriter(path, true);
-            streamWriter.WriteLine(logRecord);
+            await File.AppendAllTextAsync(path, logRecord + Environment.NewLine);
         }
     }
 }
